271247a74780bc4e2b498218128f4cd40340795f
Andreas Bjørn Hassing <anniels@microsoft.com>
2022-05-06 09:10:28 +0200

    Add sample ASA tests and execute them in CI (#7)
    * Add sample tests and execute them in CI
    
    * Fix ValidScenarioNames for ValidateSet
    
    (this took a lot longer than expected :-D)
    
    * Give up, like a boss!
    
    * Safely resolve paths
    
    * (Revert after) test failing a test
    
    * Revert "(Revert after) test failing a test"
    
    This reverts commit aa8a38eb5eea334ca27b869527989385d4f3711f.
    
    * Fix minor issues in Invoke-StreamAnalyticsTests.ps1
    
    * Make line ownership easier in Prerequisites
    
    It is assumed that we may add more lines later, I don't want
    the next developer adding to this to suddenly own the line that
    installs azure-streamanalytics-cicd

---
+++ scripts/Invoke-StreamAnalyticsTests.ps1
@@ -0,0 +1,54 @@
+<#
+.SYNOPSIS
+    Run Stream Analytics tests.
+
+.PARAMETER Scenario
+    Scenario name to run tests for.
+    If not set, the script will run all tests found in the repository.
+#>
+
+#Requires -Version 7.0
+
+param(
+    [ValidateSet(
+        # add to this as and when new scenarios are created
+        'asset-maintenance',
+        'machine-reporting-status'
+    )]
+    $Scenario
+)
+
+if (-not (Get-Command azure-streamanalytics-cicd -ErrorAction SilentlyContinue)) {
+    throw "azure-streamanalytics-cicd is not installed, install using NPM: npm install -g azure-streamanalytics-cicd"
+}
+
+function Resolve-PathSafely($Path) {
+    return $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
+}
+
+$TestOutputPath = Resolve-PathSafely -Path "$PSScriptRoot/../TestOutput"
+
+function Invoke-Test($TestConfigPath) {
+    if (-not (Test-Path -Path $TestConfigPath)) {
+        throw "Could not find test config at $TestConfigPath"
+    }
+
+    # get project path, by convention it is 1 folder up from the test config file
+    $projectPath = Resolve-Path "$TestConfigPath/../../asaproj.json"
+
+    azure-streamanalytics-cicd test -project $projectPath -testConfigPath $TestConfigPath -outputPath $TestOutputPath
+}
+
+if ($Scenario) {
+    $testConfigPath = Resolve-PathSafely -Path "$PSScriptRoot/../stream-analytics-queries/$Scenario/Test/testConfig.json"
+
+    Invoke-Test -TestConfigPath $testConfigPath
+}
+else {
+    # Run all tests
+    $testConfigPaths = Get-ChildItem -Recurse -Path "$PSScriptRoot/../stream-analytics-queries/*/testConfig.json"
+
+    foreach ($testConfigPath in $testConfigPaths) {
+        Invoke-Test -TestConfigPath $testConfigPath
+    }
+}
