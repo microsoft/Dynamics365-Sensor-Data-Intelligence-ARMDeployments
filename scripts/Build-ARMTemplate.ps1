3af15908838f242dda35ddc9a9332f8e77e02545
Andreas Bjørn Hassing <anniels@microsoft.com>
2022-05-12 09:58:07 +0000

    Enforce \n instead of \r\n logic apps

--- scripts/Build-ARMTemplate.ps1
+++ scripts/Build-ARMTemplate.ps1
@@ -25,17 +25,21 @@ foreach ($logicAppPath in (Get-ChildItem -Path "$PSScriptRoot/../logic-apps/*.js
     $logicAppDefinition.definition.parameters.PSObject.Properties | Where-Object { $_.Value.type -eq 'Object' } | ForEach-Object { $_.Value.defaultValue = @{} }
     $logicAppDefinition.definition.parameters.PSObject.Properties | Where-Object { $_.Value.type -eq 'String' } | ForEach-Object { $_.Value.defaultValue = '' }
 
-    Set-Content -Path $logicAppPath -Encoding utf8 -Value ($logicAppDefinition | ConvertTo-Json -Depth 100)
+    $logicAppDefinitionJson = ($logicAppDefinition | ConvertTo-Json -Depth 100).Replace("`r`n", "`n")
+
+    Set-Content -Path $logicAppPath -Encoding utf8 -Value $logicAppDefinitionJson
 }
 
 az bicep build `
-    --file $PSScriptRoot\..\main.bicep `
-    --outfile $PSScriptRoot\..\azuredeploy.json
+    --file "$PSScriptRoot/../main.bicep" `
+    --outfile "$PSScriptRoot/../azuredeploy.json"
 
 if ($CopyOutputToClipboard) {
     Get-Content `
-        -Path $PSScriptRoot\..\azuredeploy.json `
+        -Path "$PSScriptRoot/../azuredeploy.json" `
         -Encoding utf8 `
         -Raw `
     | Set-Clipboard
 }
+
+Write-Host "Build completed successfully"
